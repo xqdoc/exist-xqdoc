5c7c0fd56a6d2b1b40c984d241b9de23fa08822a
Duncan Paterson <d.paterson@me.com>
2020-11-11 15:34:14 +0100

    fix(app.js): navbar import

--- src/main/js/frontend/src/App.js
+++ src/main/js/frontend/src/App.js
@@ -1,22 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import NavBar from 'react-bootstrap/NavBar';
+import Navbar from 'react-bootstrap/Navbar';
 import { HashRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
       <Router>
-          <NavBar bg="dark" variant="dark" fixed="top">
-              <NavBar.Brand href="./">
+          <Navbar bg="dark" variant="dark" fixed="top">
+              <Navbar.Brand href="./">
                   <img
                       id="logo"
                       alt=""
                       src="book.svg"
                       width="80"
                       height="40"
-                  />{' '}XQuery Function Documentation</NavBar.Brand>
-          </NavBar>
+                  />{' '}XQuery Function Documentation</Navbar.Brand>
+          </Navbar>
       </Router>
   );
 }
