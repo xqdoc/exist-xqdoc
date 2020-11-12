1e282700e17026ea0a0ff683660acaf3b69b6399
lcahlander <loren.cahlander@gmail.com>
2020-11-12 14:35:17 -0500

    Adding a releases folder

--- src/main/js/frontend/src/App.js
+++ src/main/js/frontend/src/App.js
@@ -1,7 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import { HashRouter as Router } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { HashRouter as Router, Link, Route, Switch } from "react-router-dom";
 import './App.css';
 
 function App() {
@@ -17,6 +20,12 @@ function App() {
                       height="40"
                   />{' '}XQuery Function Documentation</Navbar.Brand>
           </Navbar>
+          <Container style={{marginTop: "70px"}}>
+              <Row xs={1}>
+                  <Col md={3} xl={2} xs={12} style={{border: 1, borderColor: "black", height: "100vh"}}>Foo</Col>
+                  <Col md={9} xl={8} xs={12} style={{border: 1, borderColor: "black", height: "100vh"}}>Bar</Col>
+              </Row>
+          </Container>
       </Router>
   );
 }
