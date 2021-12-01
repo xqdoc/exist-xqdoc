5c118d64014c23b4ee648a3ca91e0b0ed58b228e
lcahlander <loren.cahlander@gmail.com>
2021-12-01 11:38:11 -0500

    Adding instructions to edit the servers entry in the Swagger UI

--- src/main/js/frontend/src/Home.js
+++ src/main/js/frontend/src/Home.js
@@ -15,6 +15,7 @@ export default class Home extends Component {
                 <div>The information for this application is generated out of the xqDoc
                     files that are generated from the XQuery code and the Rest APIs
                     are generated from the RestXQ annotations within the code.</div>
+                <h2>Regeneration</h2>
                 <div>If new code has been added , then there are two ways to get
                     the xqDoc files up to date.</div>
                 <ol>
@@ -24,6 +25,10 @@ export default class Home extends Component {
                         <XMLViewer xml={xml} />
                     </li>
                 </ol>
+                <h2>RestAPIs</h2>
+                <div>In order to set the Servers dropdown value, in eXide open
+                    &nbsp;<b>/db/apps/xqdoc/data/openapi.json</b> and edit the
+                    &nbsp;<b>servers</b> entry and save it.</div>
             </>
         )
     }
